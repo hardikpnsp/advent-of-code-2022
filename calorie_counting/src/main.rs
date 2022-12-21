88ce2a22f33a9e95cf782f83e4149880f039d30a
Hardik Prajapati <hardik.prajapati@avalara.com>
2022-12-21 09:59:32 +0530

    DAY 1: calorie counter

--- calorie_counting/src/main.rs
+++ calorie_counting/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+extern crate core;
+
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+fn read_input() -> Vec<String> {
+    let file = File::open("./calorie_counting/input/input.txt").unwrap();
+    let br = BufReader::new(file);
+    let input: Vec<String> = br.lines().map(|line| line.unwrap()).collect();
+    input
+}
+
+fn process_input(input: Vec<String>) -> Vec<Vec<i64>> {
+    let mut result: Vec<Vec<i64>> = vec![];
+
+    let mut current_elf: Vec<i64> = vec![];
+    for line in input {
+        if line == "" {
+            result.push(current_elf.to_vec());
+            current_elf = vec![];
+        } else {
+            current_elf.push(line.parse().unwrap())
+        }
+    }
+    result
+}
+
+fn main() -> std::io::Result<()> {
+    let input = read_input();
+    let elves = process_input(input);
+    let mut total_calories: Vec<i64> = elves.iter().map(|elf| elf.iter().sum()).collect();
+    let max = total_calories.iter().max().unwrap();
+    println!("Part 1: highest calories in elves = {:?}", max);
+    total_calories.sort();
+    println!("Part 2: total of top 3 highest calories in elves = {:?}", total_calories.iter().rev().take(3).sum::<i64>());
+    Ok(())
 }

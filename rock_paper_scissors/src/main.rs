964e2012960e84343360f2e9ff641c2ad8c019b4
Hardik Prajapati <hardik.prajapati@avalara.com>
2022-12-21 21:38:09 +0530

    DAY 2: part 2 rock paper scissor

--- rock_paper_scissors/src/main.rs
+++ rock_paper_scissors/src/main.rs
@@ -16,11 +16,40 @@ enum Shape {
 }
 
 impl Shape {
-    pub fn new(shape: char) -> Self {
+    pub fn new(shape: &str) -> Self {
         match shape {
-            'A' | 'X' => Shape::Rock,
-            'B' | 'Y' => Shape::Paper,
-            'C' | 'Z' => Shape::Scissor,
+            "A" | "X" => Shape::Rock,
+            "B" | "Y" => Shape::Paper,
+            "C" | "Z" => Shape::Scissor,
+            _ => panic!(),
+        }
+    }
+
+    fn win(opponent: Shape) -> Shape {
+        match opponent {
+            Shape::Rock => Shape::Paper,
+            Shape::Paper => Shape::Scissor,
+            Shape::Scissor => Shape::Rock,
+        }
+    }
+
+    fn lose(opponent: Shape) -> Shape {
+        match opponent {
+            Shape::Rock => Shape::Scissor,
+            Shape::Paper => Shape::Rock,
+            Shape::Scissor => Shape::Paper,
+        }
+    }
+
+    fn draw(opponent: Shape) -> Shape {
+        opponent
+    }
+
+    pub fn from_strategy(strategy: &str, opponent: Shape) -> Self {
+        match strategy {
+            "X" => Shape::lose(opponent),
+            "Y" => Shape::draw(opponent),
+            "Z" => Shape::win(opponent),
             _ => panic!(),
         }
     }
@@ -32,11 +61,20 @@ struct Game {
 }
 
 impl Game {
-    pub fn new(game_string: String) -> Game {
+    pub fn new(game_string: &String) -> Game {
         let (opponent, player) = game_string.split_once(' ').unwrap();
         return Game {
-            player: Shape::new(player.parse().unwrap()),
-            opponent: Shape::new(opponent.parse().unwrap()),
+            player: Shape::new(player),
+            opponent: Shape::new(opponent),
+        };
+    }
+
+    pub fn new_part_2(game_string: &String) -> Game {
+        let (opponent, strategy) = game_string.split_once(' ').unwrap();
+        let opponent = Shape::new(opponent);
+        return Game {
+            player: Shape::from_strategy(strategy, opponent),
+            opponent,
         };
     }
 
@@ -67,9 +105,11 @@ impl Game {
 
 fn main() {
     let lines = read_lines(File::open("rock_paper_scissors/input/input.txt").unwrap());
+    let score: i64 = lines.iter().map(|line| Game::new(line).score()).sum();
+    println!("Part 1: highest calories in elves = {:?}", score);
     let score: i64 = lines
         .iter()
-        .map(|line| Game::new(line.parse().unwrap()).score())
+        .map(|line| Game::new_part_2(line).score())
         .sum();
     println!("Part 1: highest calories in elves = {:?}", score);
 }

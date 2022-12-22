27c86ef8f62d64d0bf79493054a2ec70e549b9a4
Hardik Prajapati <hardik.prajapati@avalara.com>
2022-12-22 11:52:41 +0530

    DAY 3: part 2 rucksack reorganization

--- rucksack_reorganization/src/main.rs
+++ rucksack_reorganization/src/main.rs
@@ -16,12 +16,29 @@ fn main() {
 
     let common_chars = lines.iter().map(|line| {
         let (left, right) = line.split_at(line.len() / 2);
-        let left:HashSet<char> = left.chars().collect();
-        let right:HashSet<char> = right.chars().collect();
+        let left: HashSet<char> = left.chars().collect();
+        let right: HashSet<char> = right.chars().collect();
         let common_char = left.intersection(&right).next().unwrap();
         common_char.clone()
     });
 
     let total: i64 = common_chars.map(|item| priority(item)).sum();
     println!("Part 1: sum of the priorities of common items {:?}", total);
+
+    let badges = lines.chunks(3).map(|chunks| {
+        let common_char = chunks
+            .iter()
+            .map(|chunk| chunk.chars().collect::<HashSet<char>>())
+            .reduce(|acc, element| acc.intersection(&element).cloned().collect())
+            .unwrap();
+
+        let common_char = common_char.iter().next().unwrap();
+        return common_char.clone();
+    });
+
+    let badges_total: i64 = badges.map(|item| priority(item)).sum();
+    println!(
+        "Part 2: sum of the priorities of common items {:?}",
+        badges_total
+    );
 }

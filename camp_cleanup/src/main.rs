1e17d37e43f0a316a001cba62fb640d99b8ca7f3
Hardik Prajapati <hardik.prajapati@avalara.com>
2022-12-23 09:17:28 +0530

    DAY 4: part 2 camp cleanup

--- camp_cleanup/src/main.rs
+++ camp_cleanup/src/main.rs
@@ -18,6 +18,10 @@ impl Interval {
     pub fn contains(&self, interval: &Interval) -> bool {
         interval.start >= self.start && interval.end <= self.end
     }
+
+    pub fn overlaps(&self, interval: &Interval) -> bool {
+        (self.start <= interval.start && interval.start <= self.end) || (self.start <= interval.end && interval.end <= self.end) || interval.contains(self)
+    }
 }
 
 fn main() {
@@ -30,4 +34,12 @@ fn main() {
     }).filter(|contained| *contained).count();
 
     println!("Part 1 - fully contained intervals: {:?}", fully_contained);
+
+    let ovelapped = lines.iter().map(|line| {
+        let (i1, i2) = line.split_once(",").unwrap();
+        let (i1, i2) = (Interval::from(i1), Interval::from(i2));
+        i1.overlaps(&i2)
+    }).filter(|contained| *contained).count();
+
+    println!("Part 2 - overlapped intervals: {:?}", ovelapped);
 }

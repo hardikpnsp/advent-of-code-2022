5f546aa747eda32eea103e4e64dca7893c2bc8a8
Hardik Prajapati <hardik.prajapati@avalara.com>
2023-01-11 12:13:05 +0530

    DAY 9: refactor to create a linked list of knots for part 2

--- rope_bridge/src/main.rs
+++ rope_bridge/src/main.rs
@@ -1,5 +1,7 @@
+use std::cell::RefCell;
 use std::collections::HashSet;
 use std::fs::File;
+use std::rc::Rc;
 use utils::read_lines;
 
 enum Direction {
@@ -42,6 +44,73 @@ impl Position {
     }
 }
 
+#[derive(Debug)]
+struct Knot {
+    position: Position,
+    next: Option<Rc<RefCell<Knot>>>,
+}
+
+impl Knot {
+    pub fn up(&mut self) {
+        self.position.up();
+        if let Some(next) = &self.next {
+            let mut next = next.borrow_mut();
+            if (self.position.y - next.position.y).abs() == 2 {
+                next.position.x = self.position.x;
+                next.up();
+            } else if (self.position.y - next.position.y).abs() > 2 {
+                panic!()
+            }
+        }
+    }
+
+    pub fn down(&mut self) {
+        self.position.down();
+        if let Some(next) = &self.next {
+            let mut next = next.borrow_mut();
+            if (self.position.y - next.position.y).abs() == 2 {
+                next.position.x = self.position.x;
+                next.down();
+            } else if (self.position.y - next.position.y).abs() > 2 {
+                panic!()
+            }
+        }
+    }
+
+    pub fn left(&mut self) {
+        self.position.left();
+        if let Some(next) = &self.next {
+            let mut next = next.borrow_mut();
+            if (self.position.x - next.position.x).abs() == 2 {
+                next.position.y = self.position.y;
+                next.left();
+            } else if (self.position.x - next.position.x).abs() > 2 {
+                panic!()
+            }
+        }
+    }
+
+    pub fn right(&mut self) {
+        self.position.right();
+        if let Some(next) = &self.next {
+            let mut next = next.borrow_mut();
+            if (self.position.x - next.position.x).abs() == 2 {
+                next.position.y = self.position.y;
+                next.right();
+            } else if (self.position.x - next.position.x).abs() > 2 {
+                panic!()
+            }
+        }
+    }
+
+    pub fn position(&self) -> Position {
+        Position {
+            x: self.position.x,
+            y: self.position.y,
+        }
+    }
+}
+
 fn process_line(line: &str) -> (Direction, i64) {
     let (direction, steps) = line.split_once(" ").unwrap();
     return (
@@ -51,53 +120,38 @@ fn process_line(line: &str) -> (Direction, i64) {
 }
 
 fn find_positions_visited_by_tail(lines: Vec<(Direction, i64)>) -> usize {
+    let tail = Rc::new(RefCell::new(Knot {
+        position: Position { x: 0, y: 0 },
+        next: None,
+    }));
+    let head = Knot {
+        position: Position { x: 0, y: 0 },
+        next: Some(tail.clone()),
+    };
+    positions_visited_by_tail(lines, tail, head)
+}
+
+fn positions_visited_by_tail(lines: Vec<(Direction, i64)>, tail: Rc<RefCell<Knot>>, mut head: Knot) -> usize {
     let mut visited = HashSet::new();
-    let mut head = Position { x: 0, y: 0 };
-    let mut tail = Position { x: 0, y: 0 };
-    visited.insert(tail);
+    visited.insert(tail.borrow().position());
 
     lines.iter().for_each(|(direction, step)| {
         for _ in 0..*step {
             match direction {
                 Direction::Up => {
                     head.up();
-                    if (head.y - tail.y).abs() == 2 {
-                        tail.x = head.x;
-                        tail.up();
-                    } else if (head.y - tail.y).abs() > 2 {
-                        panic!()
-                    }
                 }
                 Direction::Down => {
                     head.down();
-                    if (head.y - tail.y).abs() == 2 {
-                        tail.x = head.x;
-                        tail.down();
-                    } else if (head.y - tail.y).abs() > 2 {
-                        panic!()
-                    }
                 }
                 Direction::Right => {
                     head.right();
-                    if (head.x - tail.x).abs() == 2 {
-                        tail.y = head.y;
-                        tail.right();
-                    } else if (head.x - tail.x).abs() > 2 {
-                        panic!()
-                    }
                 }
                 Direction::Left => {
                     head.left();
-                    if (head.x - tail.x).abs() == 2 {
-                        tail.y = head.y;
-                        tail.left();
-                    } else if (head.x - tail.x).abs() > 2 {
-                        panic!()
-                    }
                 }
             }
-            println!("{:?} {:?}", head, tail);
-            visited.insert(tail);
+            visited.insert(tail.borrow().position());
         }
     });
 
